6e4102ed362a5ffd3a11bec6a471da8d48016622
Talha_Altair <cooltalha27@gmail.com>
2023-01-28 15:52:05 +0530

    html added for backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from business import get_data
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/api', methods=['GET'])
 def api():
